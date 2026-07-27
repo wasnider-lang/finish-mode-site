6cef66765c36ef6dc5202aec011720b0ea960a9f
wasnider-lang <wasnider@gmail.com>
2026-07-27 13:00:50 -0700

    Connect Finish Mode purchase buttons to Gumroad checkout

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ window.FINISH_MODE_CONFIG = {
   productName: "Finish Mode: The 25-Minute AI Focus Sprint Kit",
   launchPrice: "$9",
   standardPrice: "$15",
-  checkoutUrl: "",
+  checkoutUrl: "https://dailyaiproductivity.gumroad.com/l/xvbzov?wanted=true",
   deliveryProviderUrl: "https://app.gumroad.com/library",
   supportEmail: ""
 };
